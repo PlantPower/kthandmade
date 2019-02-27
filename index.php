54bb656785ae0d69d886aa0146abfe31378d9466
Jonas Edoff <jonas.edoff@gmail.com>
2019-02-27 22:10:30 +0700

    Started on the navbar. Added a logo. need to make the logo responsive

--- index.php
+++ index.php
@@ -1,47 +1,58 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title>KT HandMadeShop</title>
-    <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
-    <link href="styles/bootstrap.min.css" rel="stylesheet">
+  <meta charset="utf-8">
+    <title>KT HandMade </title>
+      <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
+      <link href="styles/bootstrap.min.css" rel="stylesheet">
+      <link href="styles/styles.css" rel="stylesheet">
+      <link href="fontawesome/css/font-awesome.min.css" rel="stylesheet">
 
-    <link href="styles/style.css" rel="stylesheet">
-
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
   </head>
   <body>
     <div id="top"><!-- top Starts -->
       <div class="container"><!-- container Starts -->
         <div class="col-md-6 offer"><!-- col-md-6 offer Starts -->
-          <a href="#" class="btn btn-success btn-sm" > Welcome: Guests</a>
-          <a href="#">Shopping Cart Total Price: $100, Total Items: 2</a>
-        </div><!-- col-md-6 ends -->
+          <a href="#" class="btn btn-success btn-sm" >Welcome: Guests</a>
+          <a href="#">Shopping Cart Total Price: $, Total Items : 2</a>
+
+        </div><!-- col-md-6 offer Ends -->
         <div class="col-md-6"><!-- col-md-6 Starts -->
           <ul class="menu"><!-- menu Starts -->
             <li>
               <a href="customer_register.php">Register</a>
             </li>
             <li>
-              <a href="chekout.php">My Account</a>
+              <a href='checkout.php'>My Account</a>;
             </li>
             <li>
               <a href="cart.php">Go to Cart</a>
             </li>
             <li>
-              <a href="chekout.php">Log In</a>
+              <a href='checkout.php'> Login </a>
             </li>
           </ul><!-- menu Ends -->
         </div><!-- col-md-6 Ends -->
       </div><!-- container Ends -->
     </div><!-- top Ends -->
 
-    <script src="js/bootstrap.min.js"></script>
+    <div class="navbar navbar-default" id="navbar"><!--navbar navbar-default starts -->
+      <div class="container"><!-- container starts -->
+        <div class="navbar-header"> <!-- navbar-header starts -->
+          <div class="navbar-brand home" href="index.php"><!-- navbar-brand home starts -->
+            <img src="images/ktlogo.jpg" alt="ktlogo" class="hidden-xs">
+            <!-- NEED TO MAKE A SMALL LOGO HERE <img src="images/ktlogoSmall" alt="ktlogosmall" class="visible-xs"> -->
+          </div><!-- navbar-brand home ends -->
+          <button type="buton" class="navbar-toggle" data-toggle="collapse" data-target="navigation">
+            <span class="sr-only">Toggle Navigation</span>
+            <i class="fa fa-align-justify"></i>
+          </button>
+        </div><!-- navbar-header ends -->
+      </div><!-- container ends -->
+    </div><!-- navbar navabardefault ends -->
+
     <script src="js/jquery.min.js"> </script>
-
-    <div class="">
-
-    </div>
+    <script src="js/bootstrap.min.js"></script>
 
   </body>
 </html>
