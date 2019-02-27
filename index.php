ca515dc94c8f1104cc7fa5198d4dbd321fb8add4
Jonas Edoff <jonas.edoff@gmail.com>
2019-02-27 12:15:05 +0700

    Basic layout in HTML and Bootstrap. File structure JS, CSS, Fonts

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>KT HandMadeShop</title>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet" >
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+
+    <link href="styles/style.css" rel="stylesheet">
+
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="top"><!-- top Starts -->
+      <div class="container"><!-- container Starts -->
+        <div class="col-md-6 offer"><!-- col-md-6 offer Starts -->
+          <a href="#" class="btn btn-success btn-sm" > Welcome: Guests</a>
+          <a href="#">Shopping Cart Total Price: $100, Total Items: 2</a>
+        </div><!-- col-md-6 ends -->
+        <div class="col-md-6"><!-- col-md-6 Starts -->
+          <ul class="menu"><!-- menu Starts -->
+            <li>
+              <a href="customer_register.php">Register</a>
+            </li>
+            <li>
+              <a href="chekout.php">My Account</a>
+            </li>
+            <li>
+              <a href="cart.php">Go to Cart</a>
+            </li>
+            <li>
+              <a href="chekout.php">Log In</a>
+            </li>
+          </ul><!-- menu Ends -->
+        </div><!-- col-md-6 Ends -->
+      </div><!-- container Ends -->
+    </div><!-- top Ends -->
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"> </script>
+
+
+  </body>
+</html>
